cdfc938c49d1559dd8f11d071343ff1c4b2869e5
rwaeng <rwaeng0421@gmail.com>
2024-03-29 13:20:42 +0900

    week 3 practice

---
+++ useFullscreen.js
@@ -0,0 +1,29 @@
+export const useFullscreen = (callback) => {
+    const element = useRef();
+    const runCb = (isFull) => {
+      if (callback && typeof callback === "function") {
+        callback(isFull);
+      }
+    };
+    const triggerFull = () => {
+      if (element.current) {
+        if (element.current.requestFullscreen) {
+          element.current.requestFullscreen();
+        } else if (element.current.mozRequestFullScreen) {
+          element.current.mozRequestFullScreen();
+        } else if (element.current.webkitRequestFullscreen) {
+          element.current.webkitRequestFullscreen();
+        } else if (element.current.msRequestFullscreen) {
+          element.current.msRequestFullscreen();
+        }
+        runCb(true);
+      }
+    };
+    const exitFull = () => {
+      document.exitFullscreen();
+      if (callback && typeof callback === "function") {
+        callback(false);
+      }
+    };
+    return { element, triggerFull, exitFull };
+  };
